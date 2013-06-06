437a4fb81e634439ad10ae3404b3acfd60c4ecad
eskerda <eskerda@gmail.com>
2013-06-06 11:23:10 +0200

    Allow parameters on update.py to make it atomical

--- update.py
+++ update.py
@@ -1,5 +1,8 @@
+import argparse
+
 from redis import Redis, ConnectionPool
 from rq import Queue
+
 from gyro import tasks
 from gyro.configuration import redis_server as redis_info
 import pybikes
@@ -7,8 +10,22 @@ import pybikes
 pool = ConnectionPool(host=redis_info['host'],port=redis_info['port'],db=0)
 q = Queue('medium', connection=Redis(connection_pool = pool))
 
-schema_files = pybikes.getDataFiles()
-schemas = [pybikes.getDataFile(f) for f in schema_files]
+parser = argparse.ArgumentParser(description='Process bike sharing networks')
+parser.add_argument('--schema', help = 'selected bike sharing scheme',
+                   default=None)
+parser.add_argument('--network', help = 'a network in the bike sharing scheme',
+                   default=None)
+args = parser.parse_args()
+
+if args.schema is None:
+    schema_files = pybikes.getDataFiles()
+    schemas = [pybikes.getDataFile(f) for f in schema_files]
+else:
+    schemas = [pybikes.getDataFile(args.schema)]
+
+
 for schema in schemas:
     for instance in schema['instances']:
-        q.enqueue(tasks.updateSystem, schema['system'], instance['tag'])
+        if args.network is None or args.network == instance['tag']:
+            print "Putting %s on an update queue!" % instance['tag']
+            q.enqueue(tasks.updateSystem, schema['system'], instance['tag'])
