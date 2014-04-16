003c4b5f91380d66a83abf2d374d1e1528163823
eskerda <eskerda@gmail.com>
2014-04-16 13:03:27 +0200

    Removes clean.py and show_stats.py (no longer relevant)

--- clean.py
+++
@@ -1,5 +0,0 @@
-from pymongo import Connection
-from gyro.configuration import db_credentials as credentials
-
-connection = Connection(credentials['host'], credentials['port'])
-connection.drop_database('citybikes')
